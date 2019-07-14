c181700b3713233a38b2d5025dccd405d1dfc7e6
Sergey Chernenko <4e.sergei@gmail.com>
2019-07-14 13:51:49 +0300

    update env + settings

--- web_application/books_catalog/settings.py
+++ web_application/books_catalog/settings.py
@@ -92,11 +92,13 @@ WSGI_APPLICATION = 'books_catalog.wsgi.application'
 #     }
 # }
 #
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'books_catalog',
-        'USER': 'postgres',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'db',
         'PORT': 5432,
     }
