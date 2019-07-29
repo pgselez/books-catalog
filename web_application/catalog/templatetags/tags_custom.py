f00d6d4000d50fc3f2bde5c945fd5621c8e16484
Sergey Chernenko <4e.sergei@gmail.com>
2019-07-29 04:45:20 +0300

    templates updates

--- web_application/catalog/templatetags/tags_custom.py
+++ web_application/catalog/templatetags/tags_custom.py
@@ -5,24 +5,8 @@ from catalog.models import Category
 register = template.Library()
 
 
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-
-
-@register.simple_tag(name='tags_categories')
-def tags_categories():
-    cats = [x for x in Category.objects.all()]
-    return list(chunks(cats, 4))
-
-
-@register.filter(name='s_upper')
-def s_upper(string):
-    r = ''
-    for n, s in enumerate(string):
-        if n % 2 == 0:
-            r += s.upper()
-        else:
-            r += s.lower()
-    return r
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    query = context['request'].GET.copy()
+    query.update(kwargs)
+    return query.urlencode()
